366c07eee9c994bfaf8bbccb11d17c3702d66dcb
psskadabala <iamp2skA>
2023-10-26 15:45:38 +0700

    problem.13

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function displayGreeting() {
     alert("Hello! Tiger returns.");
 }
+var undeclaredVariable = "i fixed the error ! :)";
+console.log(undeclaredVariable);
 document.getElementById("greetButton").addEventListener("click", displayGreeting);
