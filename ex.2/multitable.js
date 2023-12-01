c39475facfb7ffff562f81333d4c7c16f4dd797a
psskadabala <iamp2skA>
2023-12-01 15:00:13 +0700

    clean code

--- ex.2/multitable.js
+++ ex.2/multitable.js
@@ -1,24 +1,3 @@
-// var numberInput = document.getElementById("numberInput");
-// var tableResult = document.getElementById("tableResult");
-
-// document.getElementById("generateTableButton").addEventListener("click", function () {
-//     var number = parseInt(numberInput.value);
-
-//     if (isNaN(number)) {
-//         tableResult.textContent = "Please enter a valid number.";
-//     } else {
-//         var tableHtml = "<h2>Multiplication table for " + number + "<h2><ul>";
-//         for (i = 1; i <= 10; i++) {
-//             var result = number * i;
-//             tableHtml += "<li>" + number + " x " + i + "=" + result + "</li>";
-//         }
-
-//         tableHtml += "</ul>";
-//         tableResult.innerHTML = tableHtml;
-//     }
-// });
-
-
 function generateTable() {
 
     var number = document.getElementById("numberInput").value;
@@ -49,8 +28,5 @@ function generateTable() {
     var tableContainer = document.getElementById("tableContainer")
     tableContainer.innerHTML = "";
     tableContainer.appendChild(table)
-
-
-
 
 }
