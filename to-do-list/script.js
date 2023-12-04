c659910b0ca48fbd2f8b318ff92bfba5ff81377b
psskadabala <iamp2skA>
2023-12-04 15:36:15 +0700

    start-todoList.filemanagement

---
+++ to-do-list/script.js
@@ -0,0 +1,9 @@
+const todoForm = document.getElementById("todoForm");
+const titleInput = document.getElementById("titleInput");
+const descriptionInput = document.getElementById("descriptionInput");
+const todoInput = document.getElementById("todoList");
+
+function addTodo() {
+    console.log("Title:", titleInput.value);
+    console.log("Description:", descriptionInput.value);
+}
